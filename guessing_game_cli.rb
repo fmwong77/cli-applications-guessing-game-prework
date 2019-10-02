4980179d9a4b36d149588a3ba7d8346d8c3c8735
Mei Wong <fongmeiw@gmail.com>
2019-10-02 17:36:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,13 +2,13 @@ def run_guessing_game
   random_number = rand(7)
   prompt_user()
   user_input = get_user_input()
-  if user_input == random_number
-    puts "You guessed the correct number!"
+  if user_input == "exit"
+    puts "Goodbye!"
   elsif user_input != random_number
     puts "Sorry! The computer guessed #{random_number}."
-  elsif user_input == "exit"
-    puts "Goodbye!"
-  elsif
+  elsif user_input == random_number
+    puts "You guessed the correct number!"
+  else
     puts "Invalid"
   end
 end
