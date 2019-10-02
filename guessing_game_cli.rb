40e4e8c902fb515a15327203c80160ea3d3412b1
Mei Wong <fongmeiw@gmail.com>
2019-10-02 17:27:29 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -6,7 +6,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   elsif user_input != random_number
     puts "Sorry! The computer guessed #{random_number}."
-  else if user_input == "exit"
+  elsif user_input == "exit"
     puts "Goodbye!"
   end
 end
